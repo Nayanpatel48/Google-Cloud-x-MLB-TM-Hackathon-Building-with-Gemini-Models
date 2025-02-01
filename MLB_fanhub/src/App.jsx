50f1f7bc680254e0edbe05a5072a00294a3c2f5f
nayan <www.patelnayan187@gmail.com>
2025-02-01 22:30:35 +0530

    foundation created

---
+++ MLB_fanhub/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class="bg-green-700">Nayan</h1>
+    </>
+  )
+}
+
+export default App
