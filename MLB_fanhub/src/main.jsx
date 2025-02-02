568a5e545e041e3cfb6bb378285ba36e035ef3d4
nayan <www.patelnayan187@gmail.com>
2025-02-02 10:05:20 +0530

    user registration work is getting done

--- MLB_fanhub/src/main.jsx
+++ MLB_fanhub/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import Contact from './components/Contact/Contact'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // here path spelling matters not convention means upper and lower cases both are equal here
+    <Route path="/"  element={<Layout/>}>
+      <Route path="/"  element={<Home/>}/>
+      <Route path="about"  element={<About/>}/>
+      <Route path="login"  element={<Login/>}/>
+      <Route path="register"  element={<Register/>}/>
+      <Route path="contact"  element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
