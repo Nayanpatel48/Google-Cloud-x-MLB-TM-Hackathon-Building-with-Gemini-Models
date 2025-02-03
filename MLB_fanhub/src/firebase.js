44f639dc1970fbd3a0b3404e6252afe1e0a65561
nayan <www.patelnayan187@gmail.com>
2025-02-03 21:53:55 +0530

    file edited

--- MLB_fanhub/src/firebase.js
+++ MLB_fanhub/src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDW3qBMKLqvmFW_2lB0IBE_CmN9FXtoV48",
-  authDomain: "crud-project-28388.firebaseapp.com",
-  projectId: "crud-project-28388",
-  storageBucket: "crud-project-28388.firebasestorage.app",
-  messagingSenderId: "1082226481100",
-  appId: "1:1082226481100:web:d589f753011848280c6f69"
+  //add firebase project configrations
 };
 
 // Initialize Firebase
